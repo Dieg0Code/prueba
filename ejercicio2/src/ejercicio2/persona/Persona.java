73f7a4822d012da954466a36505ad2aedc11c082
DiegoObando20 <44902635+DiegoObando20@users.noreply.github.com>
2020-02-22 17:58:37 -0300

    Update Persona.java

--- ejercicio2/src/ejercicio2/persona/Persona.java
+++ ejercicio2/src/ejercicio2/persona/Persona.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio2.persona;
 
 /**
  *
- * @author alumno
+ * @author Diego
  */
 public class Persona {
     private String nombre;
